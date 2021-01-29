cfd4e728e379aacf809c361c8f3a4fbbeb8c7477
Huương0206 <lethihuong02062001@gmail.com>
2021-01-29 09:03:18 +0700

    tao migration

---
+++ database/migrations/2021_01_26_070905_create_loaitin_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLoaitinTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('loaitin', function (Blueprint $table) {
+            $table->id();
+            $table->integer('thu_tu');
+            $table->string('tieu_de');
+            $table->string('tieu_de_khong_dau');
+            $table->string('tom_tat');
+            $table->unsignedBigInteger('theloai_id');
+          // $table->foreign('theloai_id')->references('id')->on('theloai');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('loaitin');
+    }
+}
