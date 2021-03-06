8441ff503c38613a8c1dc820ecb612929cc91f00
Huương0206 <lethihuong02062001@gmail.com>
2021-03-06 09:27:58 +0700

    tao routing basic

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,25 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('foo',function(){
+    return 'hello';
+});
+Route::get('huong',function(){
+    $a=1;
+    $b=2;
+    return "huong:". ($a+$b);
+});
+Route::get('huong/{id}', function ($id){
+    return 'User'.$id;
+});
+Route::get('posts/{post}/comment{comment}',function($postId, $commentId){
+    return "Post: ".$postId."comment: ".$commentId;
+});
+Route::get('user/name?',function($name= null){
+    return $name;
+});
+Route ::prefix('admin')->group(function(){
+    Route::get('user',function(){
+        return "Text group";
+    });
+});
